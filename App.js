6b38871526b1b61592bb989ea83fc1a6687897ef
Hugo Mouchon <hugomouchonpro@gmail.com>
2023-06-15 17:34:10 +0200

    Mise en place du layout et de l'image de fond

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { s } from "./App.style";
+import hotBackground from "./assets/hot.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={hotBackground} style={s.container}>
+      <View style={s.workspace}>
+        <View><Text>Température</Text></View>
+        <View><Text>Input</Text></View>
+        <View><Text>Button</Text></View>
+      </View>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
