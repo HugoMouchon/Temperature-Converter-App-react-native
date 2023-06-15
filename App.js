f784ac4432c6d90e54ae9c56d76a0bc9d5ac4ef1
Hugo Mouchon <hugomouchonpro@gmail.com>
2023-06-15 23:58:36 +0200

    Fin du developpement de l'application de conversion de température et mise en place de commentaires

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, View } from 'react-native';
 import { s } from "./App.style";
 import hotBackground from "./assets/hot.png";
+import coldBackground from "./assets/cold.png";
 import { InputTemperature } from './components/InputTemperature/InputTemperature';
 import { TemperatureDisplay } from './components/TemperatureDisplay/TemperatureDisplay';
-import { useState } from 'react';
-import { DEFAULT_TEMPERATURE, DEFAULT_UNIT, UNITS } from './constant';
-import { getOppositUnit, convertTemperatureTo } from './services/temperature-service';
+import { useEffect, useState } from 'react';
+import { DEFAULT_TEMPERATURE, DEFAULT_UNIT } from './constant';
+import { getOppositUnit, convertTemperatureTo, isIceTemperature } from './services/temperature-service';
 import { ButtonConvert } from './components/ButtonConvert/ButtonConvert';
 
 export default function App() {
 
-  const [inputValue, setInputValue] = useState(DEFAULT_TEMPERATURE)
-  const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT)
+  // State qui stock la valeur inscrit dans l'input et mise à défaut à 0°C
+  const [inputValue, setInputValue] = useState(DEFAULT_TEMPERATURE);
+  // State qui stock l'unité de température mise par defaut en °C
+  const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT);
+  // State qui stock l'image de fond de l'application
+  const [currentBackground, setCurrentBackground] = useState();
   const oppositeUnit = getOppositUnit(currentUnit);
 
+  // Utilisation du useEffect afin de récupérer et de contrôler la valueur de l'input, si elle existe alors l'image de fond change en fonction de la température indiquée (utilisation du ternaire en destructuring)
+  useEffect(() => {
+    const temperatureAsFloat = Number.parseFloat(inputValue)
+    if (!isNaN(temperatureAsFloat)) {
+      const isColdBackground = isIceTemperature(inputValue, currentUnit)
+      setCurrentBackground(isColdBackground ? coldBackground : hotBackground)
+    }
+  }, [inputValue, currentUnit])
+
+  // fonction permettant de convertir la valeur de l'input dans l'unité auquel il appartient en son opposé.
   function getConvertedTemperature() {
     const valueAsFloat = Number.parseFloat(inputValue);
     return isNaN(valueAsFloat) ? "" : convertTemperatureTo(oppositeUnit, valueAsFloat).toFixed(1);
@@ -22,17 +37,22 @@ export default function App() {
   }
 
   return (
-    <ImageBackground source={hotBackground} style={s.container}>
+    // Image de fond de l'application
+    <ImageBackground source={currentBackground} style={s.container}>
+      {/* Fenetre principale de travail */}
       <View style={s.workspace}>
+        {/* Affichage de la conversion de la temperature */}
         <TemperatureDisplay
           value={getConvertedTemperature()}
           unit={getOppositUnit(currentUnit)}
         />
+        {/* Champs Input permettant à l'utilisateur de taper son nombre */}
         <InputTemperature
           onChangeText={setInputValue}
           defaultValue={DEFAULT_TEMPERATURE}
           unit={currentUnit}
         />
+        {/* Bouton de conversion Celcius vers Faraneigt et inversement */}
         <ButtonConvert
           onPress={() => {
             setCurrentUnit(oppositeUnit);
@@ -43,4 +63,3 @@ export default function App() {
     </ImageBackground>
   );
 }
-
--- services/temperature-service.js
+++ services/temperature-service.js
@@ -1,15 +1,26 @@
 import { UNITS } from "../constant";
 
+// fonction permettant de récupérer l'unité opposé.
 function getOppositUnit(unit) {
     return unit == UNITS.celcius ? UNITS.faranheigt : UNITS.celcius;
 }
 
+// fonction permettant de convertire des degrés celcius en faraneigt et inversement.
 function convertTemperatureTo(unit, value) {
-    if(unit == UNITS.celcius) {
-        return(value - 32) / 1.8;
-    }else{
+    if (unit == UNITS.celcius) {
+        return (value - 32) / 1.8;
+    } else {
         return value * 1.8 + 32;
     }
 }
 
-export {getOppositUnit, convertTemperatureTo };
+// fonction permettant de savoir si la valeur inscrite est négative (soit en °C ou en °F)
+function isIceTemperature(value, unit) {
+    if (unit == UNITS.celcius) {
+        return value <= 0
+    } else {
+        return value <= 32
+    }
+}
+
+export { getOppositUnit, convertTemperatureTo, isIceTemperature };
